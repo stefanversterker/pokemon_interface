13ee9d7f2c319ac6966a177ef03c003b1fce7367
Rowan Plooij <77756218+RowanPl@users.noreply.github.com>
2025-10-17 11:20:23 +0200

    Fix typo in comment
    Corrected a typo in a comment "foutmeldigen" to "foutmeldingen"

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
     private static List<Pokemon> pokemons = Arrays.asList(charizard, blastoise, venusaur, ditto, raichu, gyarados);
 
 
-    // Los in de main methode alle foutmeldigen op door (abstracte) klassen met variabelen en methoden te maken (en soms een import).
+    // Los in de main methode alle foutmeldingen op door (abstracte) klassen met variabelen en methoden te maken (en soms een import).
     // In de main methode en de Main klasse hoef je niks te veranderen.
     public static void main(String[] args) {
         Scanner speler_A = new Scanner(System.in);
